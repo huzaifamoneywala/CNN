7e5edbb21b476178ae4624c7c7348f49072cb4af
huzaifamoneywala <huzaifamoneywala@gmail.com>
2025-08-27 18:29:15 +0500

    Create admin_header.php

---
+++ admin_header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Admin Panel</title>
+    <link rel="stylesheet" href="../css/styles.css">
+</head>
+<body>
+<nav>
+    <a href="index.php">Dashboard</a> |
+    <a href="articles.php">Manage Articles</a> |
+    <a href="categories.php">Manage Categories</a>
+</nav>
+<hr>
