1d3c911d6742155569ffe08489d1748842bdabf6
huzaifamoneywala <huzaifamoneywala@gmail.com>
2025-08-27 19:10:48 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php include '../includes/db.php'; ?>
+<?php include 'includes/admin_header.php'; ?>
+
+<h1>Welcome to Admin Panel</h1>
+<ul>
+    <li><a href="articles.php">Manage Articles</a></li>
+    <li><a href="categories.php">Manage Categories</a></li>
+</ul>
+
+</body>
+</html>
